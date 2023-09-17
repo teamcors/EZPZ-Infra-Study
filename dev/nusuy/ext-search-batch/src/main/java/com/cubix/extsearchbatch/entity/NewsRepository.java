ad99871e9a437a17218caf0a03201ef191f0ba06
Yusun Choi <nsuy.ch@gmail.com>
2023-09-17 12:41:05 +0900

    fix: Change `findByTitleAndUrlAndPublishedAtWithJPQL` to `findTop1ByTitle` (#1)

--- dev/nusuy/ext-search-batch/src/main/java/com/cubix/extsearchbatch/entity/NewsRepository.java
+++ dev/nusuy/ext-search-batch/src/main/java/com/cubix/extsearchbatch/entity/NewsRepository.java
@@ -1,16 +1,7 @@
 package com.cubix.extsearchbatch.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.time.LocalDateTime;
 
 public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
-    @Query("select n from NewsEntity n " +
-            "where trim(n.title) = trim(:title) " +
-            "and n.description = :description " +
-            "and n.url = :url " +
-            "and n.publishedAt = :publishedAt " +
-            "order by n.publishedAt desc limit 1")
-    NewsEntity findByTitleAndUrlAndPublishedAtWithJPQL(String title, String url, String description, LocalDateTime publishedAt);
+    NewsEntity findTop1ByTitle(String title);
 }
