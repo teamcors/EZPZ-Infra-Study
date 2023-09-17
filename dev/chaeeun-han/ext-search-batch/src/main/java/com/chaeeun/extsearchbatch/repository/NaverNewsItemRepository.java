c63c406de4510191ba705665d4f8d8575743cd7f
chaeeun-Han <chaeeun.h911@gmail.com>
2023-09-17 11:28:43 +0900

    refactor: Change `NewsInfoRepository` to `NaverNewsItemRepository` (#1)

--- dev/chaeeun-han/ext-search-batch/src/main/java/com/chaeeun/extsearchbatch/repository/NaverNewsItemRepository.java
+++ dev/chaeeun-han/ext-search-batch/src/main/java/com/chaeeun/extsearchbatch/repository/NaverNewsItemRepository.java
@@ -3,6 +3,6 @@ package com.chaeeun.extsearchbatch.repository;
 import com.chaeeun.extsearchbatch.domain.NaverNewsItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NewsInfoRepository extends JpaRepository<NaverNewsItem,Long> {
+public interface NaverNewsItemRepository extends JpaRepository<NaverNewsItem,Long> {
     NaverNewsItem findByTitle(String title);
 }
