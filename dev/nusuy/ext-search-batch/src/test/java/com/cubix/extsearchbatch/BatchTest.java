e074d696862ca102cd2293c096b2a06fa6c40287
Yusun Choi <nsuy.ch@gmail.com>
2023-09-20 19:54:38 +0900

    test: Add news save test (#3)

---
+++ dev/nusuy/ext-search-batch/src/test/java/com/cubix/extsearchbatch/BatchTest.java
@@ -0,0 +1,50 @@
+package com.cubix.extsearchbatch;
+
+import com.cubix.extsearchbatch.dto.NaverRawNewsItemDto;
+import com.cubix.extsearchbatch.entity.NewsEntity;
+import com.cubix.extsearchbatch.entity.NewsRepository;
+import com.cubix.extsearchbatch.service.DataUpdateService;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@Transactional
+@DisplayName("ext-search-batch Test")
+@SpringBootTest
+public class BatchTest {
+    private final DataUpdateService dataUpdateService;
+    private final NewsRepository newsRepository;
+
+    @Autowired
+    public BatchTest(DataUpdateService dataUpdateService, NewsRepository newsRepository) {
+        this.dataUpdateService = dataUpdateService;
+        this.newsRepository = newsRepository;
+    }
+
+    @DisplayName("적재 데이터량 테스트")
+    @Test
+    public void quantityTest() {
+        // given
+        long expectedRawSize = 1000;
+
+        // when
+        dataUpdateService.updateNewsData();
+
+        // then
+        ArrayList<NaverRawNewsItemDto> rawData = dataUpdateService.getRawData();
+        ArrayList<NewsEntity> resultData = dataUpdateService.getResultData();
+
+        assertThat(rawData.size()).isEqualTo(expectedRawSize);
+        for (NewsEntity news : resultData) {
+            assertThat(newsRepository.findTop1ByTitle(news.getTitle())).isNotNull();
+        }
+    }
+}
