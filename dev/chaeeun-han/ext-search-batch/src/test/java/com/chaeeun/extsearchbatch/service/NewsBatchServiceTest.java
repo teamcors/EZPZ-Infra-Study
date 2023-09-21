c918ba7f63960e8735ebcd2b0a3ba9559e97d24c
chaeeun-Han <chaeeun.h911@gmail.com>
2023-09-21 14:05:20 +0900

    chore: Upgrade JUnit version to 5 (#3)

--- dev/chaeeun-han/ext-search-batch/src/test/java/com/chaeeun/extsearchbatch/service/NewsBatchServiceTest.java
+++ dev/chaeeun-han/ext-search-batch/src/test/java/com/chaeeun/extsearchbatch/service/NewsBatchServiceTest.java
@@ -2,16 +2,15 @@ package com.chaeeun.extsearchbatch.service;
 
 import com.chaeeun.extsearchbatch.domain.NaverNewsItem;
 import com.chaeeun.extsearchbatch.repository.NaverNewsItemRepository;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
@@ -20,7 +19,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(properties = {"spring.config.location = classpath:application.yml"})
 public class NewsBatchServiceTest {
 
@@ -36,7 +34,7 @@ public class NewsBatchServiceTest {
     @Value("${NAVER.CLIENT_SECRET}")
     private String CLIENT_SECRET;
 
-    @Before
+    @BeforeEach
     public void setup() {
         MockitoAnnotations.initMocks(this);
 
