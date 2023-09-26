b81535a8a898a3b87580c947c962783ff7aae438
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2023-09-26 12:29:32 +0900

    test: Use naverNewsApi from NaverNewsService (#3)

--- dev/choihyojeong/ext-search-batch/src/test/java/com/choihyojeong/extsearchbatch/NaverNewsBatchTest.java
+++ dev/choihyojeong/ext-search-batch/src/test/java/com/choihyojeong/extsearchbatch/NaverNewsBatchTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cglib.core.Local;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,6 +17,8 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.client.RestTemplate;
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,6 +37,8 @@ class NaverNewsBatchTest {
 
     @Autowired
     NaverNewsRepository NaverNewsRepoTest;
+    @Autowired
+    NaverNewsService NaverNewsService;
     
     @Value("${naver.client.id}")
     private String CLIENT_ID;
@@ -46,110 +52,81 @@ class NaverNewsBatchTest {
     @Value("${naver.base.path}")
     private String PATH;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateDate;
+
+
     private RestTemplate restTemplate = new RestTemplate();
 
-    List<String> newNewsTitle = new ArrayList<>(); //새로 적재한 뉴스 title
+    int success = 0;
 
     @DisplayName("BatchTest: 새로운 뉴스와 적재된 뉴스의 개수가 같다.")
     @Test
     public void getBatchNewsNumber() throws Exception {
-
         //given
-        // Naver 검색 API 호출
-        System.out.println("given-Naver 검색 API 호출 시작");
-        URI BUrl = UriComponentsBuilder
-                .fromUriString(BASE_URL)
-                .path(PATH)
-                .queryParam("query", "주식")
-                .queryParam("display", 10)
-                .queryParam("start", 1)
-                .queryParam("sort", "date") //issue #2
-                .encode()
-                .build()
-                .toUri();
-
-        MultiValueMap<String, String> httpConnect;
-        NaverResultDTO resultDTO = restTemplate.exchange(
-                BUrl, HttpMethod.GET,
-                new HttpEntity<>(httpConnect()),
-                NaverResultDTO.class
-        ).getBody();
-        List<NaverNewsItem> newsItem = resultDTO.getItems();
-        List<NaverNewsItem> saveItem = new ArrayList<>(); //한번에 저장하기 위한 배열
-
-        int newNewsLen = 0; //새로운 뉴스 개수
         int saveNewsLen = 0; // DB에 저장된 뉴스 개수
+        NaverNewsService.naverNewsApi();
 
-        //When
-        //반환 결과 DB에 저장
-        for (NaverNewsItem news : newsItem) {
-
-            //기존에 있는 데이터 = findNews
-            //System.out.println("1"+news);
-            NaverNewsItem findNews = NaverNewsRepoTest.findByTitle((news.getTitle()));
+        updateDate = LocalDateTime.now();
+        String checkUpDate = updateDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        LocalDateTime currentDate = LocalDateTime.parse(checkUpDate,DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
 
-            String newNews = news.getLink();
-
-
-            if (findNews == null) {
-                saveItem.add(news);
-                System.out.println("뉴스 데이터 적재 완료");
-                newNewsTitle.add(news.getTitle()); //새로운 뉴스 제목만 저장
-                newNewsLen = newNewsLen + 1;
-
-            } else {
-                String oldNews = findNews.getLink();
-                if (!oldNews.equals(newNews)) {
-                    findNews.setDescription(news.getDescription());
-                    findNews.setLink(news.getLink());
-                    findNews.setOriginallink(news.getOriginallink());
-                    findNews.setPubDate(news.getPubDate());
-
-
-                    saveItem.add(findNews);
-                    newNewsTitle.add(findNews.getTitle()); //새로운 뉴스 제목만 저장
-                    newNewsLen = newNewsLen + 1;
-
-                    System.out.println("뉴스 데이터 업데이트 완료 :  "+findNews.getTitle());
-                }
-                else {
-                    System.out.println("최신 뉴스입니다 :  " +findNews.getTitle());
-                }
-            }
-            NaverNewsRepoTest.saveAll(saveItem);
-        }
-
-        //Then
-        //반환 결과 중 새로운 뉴스 개수와 DB에 적재된 뉴스 개수가 같은지 (0~10)
-        //NaverNewsItem chekUpdate = NaverNewsRepoTest.findByTitle((news.getTitle()));
-
+        //when
         // DB에 적재된 뉴스 개수를 불러올 방법을 생각해야함
         // 1. 최근 10개를 불러와 (id로)
         List<NaverNewsItem> findtop10 = NaverNewsRepoTest.findTop10ByOrderByIdDesc(); //id 내림차순으로 top 10개 가져옴
         List<String> latestTitle = findtop10.stream().map(NaverNewsItem::getTitle).collect(Collectors.toList()); //top 10개에서 title만 구함
+        List<LocalDateTime> latestDate = findtop10.stream().map(NaverNewsItem::getUpdated_at).collect(Collectors.toList());
 
-        // 2. 불러온 10개 중에 뉴스 제목이 같은게 있는지
-
-        if(newNewsTitle != null) //새로 저장된 뉴스들이 있을 떄
-        {
-            for(int i=1; i<=newNewsTitle.size() ; i++){ // 새로 저장된 뉴스들 개수만큼 반복해서
-                if(latestTitle.contains(newNewsTitle.get(i-1))){ //제대로 저장됐는지 확인
-                    System.out.println("새로 적재된 뉴스입니다 :  " +newNewsTitle.get(i-1));
-                    saveNewsLen = saveNewsLen +1;
-                }
+        // 2. 불러온 10개 중에 업데이트 된 시간이 현재시간인지
+        for(int i=0; i<10; i++){
+            if(latestDate.get(i).equals(currentDate) || latestDate.get(i).equals(currentDate.plusSeconds(1)) || latestDate.get(i).equals(currentDate.minusSeconds(1))){
+                System.out.println("새로 적재된 뉴스입니다 : " +latestTitle.get(i));
+                saveNewsLen = saveNewsLen +1;
+            }
+            else{
+                System.out.println("새로 적재된 뉴스가 아닙니다 : "+latestTitle.get(i));
             }
         }
-        else { //새로 저장된 뉴스들이 없을 때
-            System.out.println("새로 적재된 뉴스가 없습니다.");
-        }
-
-        System.out.println("새로운 뉴스 개수" + newNewsLen);
         System.out.println("적재된 뉴스 개수"+ saveNewsLen);
 
-        // 3. 그 개수랑 새로운 뉴스 개수랑 같은지 확인하기
-        Assertions.assertThat(newNewsLen).isEqualTo(saveNewsLen);
+        //then
+        // 3. 그 개수가 0에서 10사이인지
+        Assertions.assertThat(saveNewsLen).isBetween(0,10);
 
+    }
+
+    @DisplayName("KeywordTest: 적재된 뉴스 title, description에 '주식'이 들어가있다")
+    @Test
+    public void checkKeyword() throws Exception {
+
+        //given
+        List<NaverNewsItem> findtop10 = NaverNewsRepoTest.findTop10ByOrderByIdDesc(); //id 내림차순으로 top 10개 가져옴
+
+        //top 10개에서 title만 구함
+        // 1. findtop10 list에서 gettitle로 title만 받아와 새로운 list로 저장
+        List<String> latestTitle = findtop10.stream().map(NaverNewsItem::getTitle).collect(Collectors.toList());
+
+        //when
+        for (int i = 0; i < 10; i++) {
+            // 1. 최신 10개의 뉴스에
+            // 2.decription과 title에 '주식' 키워드가 있는지
+            if(latestTitle != null) {
+                NaverNewsItem savedNews = NaverNewsRepoTest.findByTitle((latestTitle.get(i)));
+                if (savedNews.getTitle().contains("주식")) {
+                    success = success +1;
+                } else if (savedNews.getDescription().contains("주식")) {
+                    success = success +1;
+                }
+            }
+            else {
+                System.out.println("적재된 뉴스 데이터가 없습니다");
+            }
+        }
+
+        //then
+        Assertions.assertThat(success).isEqualTo(10);
     }
     private MultiValueMap<String, String> httpConnect() {
         final HttpHeaders headers = new HttpHeaders();
